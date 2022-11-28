893bb89b9958733b15118a34c2841ed2c67f7190
yokota809 <117797374+yokota809@users.noreply.github.com>
2022-11-28 23:58:52 +0900

    Update retina_spike.py

--- retina_spike.py
+++ retina_spike.py
@@ -2,26 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 count=0
 # スパイクパラメータ
-dt = 0.5; T = 200 # ms
+dt = 0.5; T = 400 # ms
 nt = round(T/dt) # シミュレーションステップ数
 t1 = np.arange(nt)*dt
 v_arr = np.zeros(nt) # 膜電位を記録する配列
 u_arr = np.zeros(nt) # 回復変数を記録する配列
 count1=np.zeros(nt)
 C = 23 # 膜容量 (pF)
-a_spike = 0.02 # 回復時定数の逆数 (1/ms)
-b_spike = 0.2 # u の v に対する共鳴度合い (pA/mV)
-c_spike=-65
-d_spike = 8 # 発火で活性化される正味の外向き電流 (pA)
-vrest = 50 # 静止膜電位 (mV)
-vreset = 60 # リセット電位 (mV)
+# a_spike = 0.02 # 回復時定数の逆数 (1/ms)
+# b_spike = 0.2 # u の v に対する共鳴度合い (pA/mV)
+# c_spike=-65
+# d_spike = 8 # 発火で活性化される正味の外向き電流 (pA)
+# vrest = 50 # 静止膜電位 (mV)
+# vreset = 60 # リセット電位 (mV)
+# vthr = -43 # 閾値電位 (mV)
+# vpeak = 30 #　ピーク電位 (mV)
+
+a_spike = 0.043 # 回復時定数の逆数 (1/ms)
+b_spike = -0.4 # u の v に対する共鳴度合い (pA/mV)
+c_spike= -52
+d_spike = 56 # 発火で活性化される正味の外向き電流 (pA)
+k_spike = 1
+vreset = -51.8 # リセット電位 (mV)
 vthr = -43 # 閾値電位 (mV)
 vpeak = 30 #　ピーク電位 (mV)
-g_arr = np.zeros(nt)
-v_old=-65.75
-u_old=7.76
-v = 0; u = 0
 
+g_arr = np.zeros(nt)
+v_old=-60
+u_old=0
+# v_old=-65.75
+# u_old=7.76
+v = 0; u = 0
+i_input=np.float64(60)
+# i_input=15.875,np.float64
+x = round(i_input, 1)
+# print(x)
+# I=0,np.float64
+# print(type(i_input))
 for i in range(0, nt):
     
     #インパルス応答変化一定間隔応答
@@ -30,30 +47,37 @@ for i in range(0, nt):
     if 0<=count<=50:
     # if count>=500:
     # # if i%50==0:
-        I=15.875
+        # I=i_input
+        I=0
         # g2=7
-    elif 100<=count<=150:
-        I=15.875
-    elif 200<=count<=250:
-        I=15.875
-    elif 300<=count<=350:
-        I=15.875
+    elif 200<=count<=600:
+        I=i_input
+        
+    # elif 200<=count<=250:
+    #     I=i_input
+    # elif 300<=count<=350:
+    #     I=i_input
     # elif 900<=count<=1000:
     #     g2=-15.875
     else:
-        I=-15.875
+        # I=-1*i_input
+        I=0
         # g2=0
-        
-
+    # print(type(i_input))
+    # print(type(I))
     # スパイク発火オイラー
-    v=v_old+(0.04*v_old*v_old+5*v_old+140-u_old+I)*(dt)
-    # v=v_old+(0.04*v_old*v_old+5*v_old+140-u_old+g2)*(dt/Cm)
-    u=u_old+a_spike*(b_spike*v_old-u_old)*dt
-
+    # v=v_old+(0.04*v_old*v_old+5*v_old+140-u_old+I)*(dt)
+    # u=u_old+a_spike*(b_spike*v_old-u_old)*dt
+    
+    v=v_old+(dt/C)*(k_spike*(v_old - vreset)*(v_old - vthr) - u_old +I)
+    u=u_old+dt*a_spike*(b_spike*(v_old-vreset)-u_old)
+    
+    # print(type(v))
+    # print(type(u))
     # v=v_old+(dt/Cm)*((v_old+51.8)*(v_old+43)+(g2-u_old))
     # u=u_old+dt*((-0.043*0.4)*(v_old+51.8)-u_old)
 
-    if v>=30:
+    if v>=vpeak:
         # v=-52
         v=c_spike
         # u=u-56

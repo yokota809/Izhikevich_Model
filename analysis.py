e2c1119c4c7f08775a4d1cd5a19ad0ad55df662d
yokota809 <117797374+yokota809@users.noreply.github.com>
2022-12-07 17:21:36 +0900

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,13 +1,14 @@
 import numpy as np
 import cv2
-
-visual_input_append=np.load("visual_input_append.npy")
-spike_data=np.load("spike_data.npy")
+import matplotlib.pyplot as plt
+visual_input_append=np.load("visual_input_append1207.npy")
+spike_data=np.load("spike_data1207.npy")
 
 framenum=visual_input_append.shape[0]
 calcunum=spike_data.shape[0]
 RF_TIME_LENGTH=10
 
+data=np.zeros(10,np.float32)
 spike_count=0
 # スパイクが発火したタイミングを捜索し、100ms前の値を取得する。
 for i in range(calcunum):
@@ -30,6 +31,7 @@ print(spike_count)
 for i in range(10):
     staforviewer=cv2.resize(sta_result[i,:,:]*2,[300,300])
     cv2.imshow('sta',staforviewer)
+    data[i]=sta_result[i,50,50]
 
 
 
@@ -37,3 +39,9 @@ for i in range(10):
     key =cv2.waitKey(1000)
     if key == 27:
         break
+t1= np.arange(10)
+np.savetxt('data1207.csv',data , delimiter=",",fmt='%12.6f')  
+plt.plot(t1,data, color="k")
+plt.xlabel('Time (ms)')
+plt.ylabel('Response')
+plt.show()
